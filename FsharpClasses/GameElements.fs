51e276ac04e2fd642fad838e05d3bfcc9e92d320
Reinhard Kuhn <reinhard.kuhn@outlook.com>
2019-01-03 13:05:30 +0100

    1st

--- FsharpClasses/GameElements.fs
+++ FsharpClasses/GameElements.fs
@@ -3,6 +3,7 @@ namespace GameElements
 open System.Windows.Controls
 open System.Drawing
 open System.Windows.Media
+open System.Diagnostics
 
 type Player =
     | None
@@ -23,6 +24,7 @@ module GameUi =
         square.Fill    <- SolidColorBrush(color)
         square.Stroke  <- SolidColorBrush(Colors.Black)
         square.StrokeThickness <- 1.0
+
         square
 
     let addSquareToCanvas (panel:Panel, square:Rectangle,  row, column) =
@@ -35,33 +37,6 @@ module GameUi =
        addSquareToCanvas( panel, square, row, column)
         |> ignore
 
-module GamePosition =
-
-    let Init (gamePosition:Player[,]) =
-        for row = 0 to 19 do
-            for col = 0 to 19 do
-               gamePosition.[row,col] <- Player.P1
-
-    let Display ( panel: Panel, gamePosition: Player[,], colorMap: Map<Player,Color>) =
-        for row = 0 to 19 do
-            for col = 0 to 19 do
-               GameUi.DrawSquare(panel, float row , float col, colorMap.[gamePosition.[row, col]]);
-
-module Game =
-    let ColorMap =
-        Map.empty
-            .Add(Player.None, Colors.White)
-            .Add(Player.P1, Colors.Red)
-            .Add(Player.P2, Colors.Blue)
-            .Add(Player.P3, Colors.Green)
-
-    let Draw (panel: Panel) =
-        let position = Array2D.create<Player>  20 20 Player.None
-
-        GamePosition.Init position
-
-        GamePosition.Display( panel, position, ColorMap)
-
 module GamePiece =
 
     type Edge =
@@ -70,145 +45,218 @@ module GamePiece =
         | Down
         | Left
 
+    type Rotation =
+        | R0
+        | R90
+        | R180
+        | R270
+
     type Piece =
         struct
             val Flip : bool
+            val Rotation : Rotation
             val Contour : Edge list
-            new (contour) = { Flip = false; Contour=contour}
+            new (contour) = { Flip = false; Rotation = R0; Contour=contour}
         end
 
-    let Flip edge =
+    let Flip(p : Piece) =
+        p.Flip = not p.Flip
+
+    let FlipEdge edge =
         match edge with
             | Up -> Up
             | Down -> Down
             | Left -> Right
             | Right -> Left
 
-    let Rotate edge =
+    let RotateEdge edge =
         match edge with
             | Up -> Right
             | Down -> Left
             | Left -> Up
             | Right -> Down
 
+    let Next( r : Rotation) =
+        match r with
+        | R0 -> R90
+        | R90 -> R180
+        | R180 -> R270
+        | R270 -> R90
+
+    let Rotate(p : Piece) =
+        p.Rotation = Next(p.Rotation)
+
+    let Log( e: Edge) =
+        match e with
+        | Up -> "Up"
+        | Down -> "Down"
+        | Right -> "Right"
+        | Left -> "Left"
+
+    let Render ( p: Piece, pos: Player[,]) =
+        p.Contour |> List.map(fun x -> Debug.WriteLine(Log(x)))
+                  |> ignore
+
 // 1
     let p_1_1 =
-        [Edge.Up, Edge.Right, Edge.Down, Edge.Left]
+        [Edge.Up; Edge.Right; Edge.Down; Edge.Left]
 
 // 2
     let p_2_1 =
-        [Edge.Up, Edge.Up, Edge.Right, Edge.Down, Edge.Down,Edge.Left]
+        [Edge.Up; Edge.Up; Edge.Right; Edge.Down; Edge.Down;Edge.Left]
 
 // 3
-    // straight
     let p_3_1 =
-        [Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down;
          Edge.Left]
 
     let p_3_2 =
-        [Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down,
-         Edge.Right,
-         Edge.Down,
-         Edge.Left,
-         Edge.Left]
-
-    let p_3_3 =
-        [Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down,
-         Edge.Right,
-         Edge.Down,
-         Edge.Left,
+        [Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down;
+         Edge.Right;
+         Edge.Down;
+         Edge.Left;
          Edge.Left]
 
 // 4
     // straight
     let p_4_1 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down,Edge.Down, Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down;Edge.Down; Edge.Down;
          Edge.Left]
 
     let p_4_2 =
-        [Edge.Up, Edge.Up, Edge.Right, Edge.Down, Edge.Right,Edge.Down,Edge.Left,Edge.Left]
+        [Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down;
+         Edge.Right;
+         Edge.Down;
+         Edge.Left;
+         Edge.Down;
+         Edge.Left]
 
     let p_4_3 =
-        [Edge.Up, Edge.Up, Edge.Right, Edge.Down, Edge.Right,Edge.Down,Edge.Left,Edge.Left]
+        [Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down;
+         Edge.Right;
+         Edge.Down;
+         Edge.Left;Edge.Left]
 
     let p_4_4 =
-        [Edge.Up, Edge.Up, Edge.Right, Edge.Down, Edge.Right,Edge.Down,Edge.Left,Edge.Left]
+        [Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down;
+         Edge.Right;
+         Edge.Down;
+         Edge.Left;Edge.Left]
 
     let p_4_5 =
-        [Edge.Up, Edge.Up, Edge.Right, Edge.Down, Edge.Right,Edge.Down,Edge.Left,Edge.Left]
+        [Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down;
+         Edge.Right;
+         Edge.Down;
+         Edge.Left;Edge.Left]
 
     let p_4_6 =
-        [Edge.Up, Edge.Up, Edge.Right, Edge.Down, Edge.Right,Edge.Down,Edge.Left,Edge.Left]
+        [Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down;
+         Edge.Right;
+         Edge.Down;
+         Edge.Left;
+         Edge.Left]
 
 // 5
-
-    // straight
     let p_5_1 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_2 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_3 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_4 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_5 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_6 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_7 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_8 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down; Edge.Down;
          Edge.Left]
 
     let p_5_9 =
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
     let p_5_10=
-        [Edge.Up, Edge.Up, Edge.Up, Edge.Up, Edge.Up,
-         Edge.Right,
-         Edge.Down, Edge.Down, Edge.Down, Edge.Down,Edge.Down,
+        [Edge.Up; Edge.Up; Edge.Up; Edge.Up; Edge.Up;
+         Edge.Right;
+         Edge.Down; Edge.Down; Edge.Down; Edge.Down;Edge.Down;
          Edge.Left]
 
-        //
+module GamePosition =
+
+    let Init (gamePosition:Player[,]) =
+        GamePiece.Render(new GamePiece.Piece(GamePiece.p_3_2), gamePosition)
+        for row = 0 to 19 do
+            for col = 0 to 19 do
+               gamePosition.[row,col] <- Player.None
+
+    let Display ( panel: Panel, gamePosition: Player[,], colorMap: Map<Player,Color>) =
+        for row = 0 to 19 do
+            for col = 0 to 19 do
+               GameUi.DrawSquare(panel, float row , float col, colorMap.[gamePosition.[row, col]]);
+module Game =
+
+    let ColorMap =
+        Map.empty
+           .Add(Player.None, Colors.White)
+           .Add(Player.P1, Colors.Red)
+           .Add(Player.P2, Colors.Blue)
+           .Add(Player.P3, Colors.Green)
+
+    let Draw (panel: Panel) =
+        let position = Array2D.create<Player>  20 20 Player.None
+
+        GamePosition.Init position
+
+        GamePosition.Display( panel, position, ColorMap)
---
+++ Game/Game.fs
@@ -0,0 +1,4 @@
+amespace Game
+open System.Windows.Controls
+open System.Drawing
+open System.Windows.Media
